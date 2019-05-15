014dd0747552e44462092c85bd6e7a997b608500
Alexander Schelchere <ascellie@gmail.com>
2019-05-15 10:39:28 -0400

    alan kay - known for GUI

--- nested.rb
+++ nested.rb
@@ -88,7 +88,7 @@ end
 
 def changing_alan
 	programmer_hash = 
- 		{
+ 		{ 
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -105,7 +105,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+
+     programmer_hash
 end
 
 def adding_to_dennis
